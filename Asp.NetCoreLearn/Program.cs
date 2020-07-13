0baed0bf0778e690ade88c1b0af17b60f25065c3
卫星 花 <892772267@qq.com>
2020-07-13 14:19:01 +0800

    新建项目

---
+++ Asp.NetCoreLearn/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Asp.NetCoreLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

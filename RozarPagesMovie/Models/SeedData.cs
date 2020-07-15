c5909fa3a5c69ffe663e1b3858a04ca98a53da9a
卫星 花 <892772267@qq.com>
2020-07-15 13:50:33 +0800

    更新页面

---
+++ RozarPagesMovie/Models/SeedData.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Razor;
+using RozarPagesMovie.Data;
+
+namespace RozarPagesMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RozarPagesMovieContext(
+                serviceProvider.GetRequiredService<DbContextOptions<RozarPagesMovieContext>>()))
+            {
+                //Look for any movie
+                if (context.Movie.Any())
+                {
+                    return;
+                }
+                context.AddRange(
+                        new Movie
+                        {
+                            Title = "When Harry Met Sally",
+                            ReleaseDate = DateTime.Parse("1989-2-12"),
+                            Genre = "Romantic Comedy",
+                            Price = 7.99M
+                        },
+                        new Movie
+                        {
+                            Title = "Ghostbusters ",
+                            ReleaseDate = DateTime.Parse("1984-3-13"),
+                            Genre = "Comedy",
+                            Price = 8.99M
+                        },
+                        new Movie
+                        {
+                            Title = "Ghostbusters 2",
+                            ReleaseDate = DateTime.Parse("1986-2-23"),
+                            Genre = "Comedy",
+                            Price = 9.99M
+                        },
+                        new Movie
+                        {
+                            Title = "Rio Bravo",
+                            ReleaseDate = DateTime.Parse("1959-4-15"),
+                            Genre = "Western",
+                            Price = 3.99M
+                        }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- RozarPagesMovie/Program.cs
+++ RozarPagesMovie/Program.cs
@@ -4,7 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RozarPagesMovie;
+using RozarPagesMovie.Models;
 using Microsoft.Extensions.Logging;
 
 namespace RozarPagesMovie
@@ -13,7 +17,21 @@ namespace RozarPagesMovie
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scop = host.Services.CreateScope())
+            {
+                var service = scop.ServiceProvider;
+                try
+                {
+                    SeedData.Initialize(service);
+                }
+                catch ( Exception e)
+                {
+                    var logger = service.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(e, "An error occurred seeding the DB.");
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>

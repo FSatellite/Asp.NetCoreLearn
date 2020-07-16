e1a0fd71539a4f5a38f9106759ca5bf1598b71c2
卫星 花 <892772267@qq.com>
2020-07-16 15:33:55 +0800

    js调用Api

--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -32,6 +32,9 @@ namespace TodoApi
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseHttpsRedirection();
 
             app.UseRouting();

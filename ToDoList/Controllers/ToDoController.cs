e7d07efc7e5771febcc941bf047b6a3edae573bd
FSatellite <892772267@qq.com>
2020-07-19 10:34:35 +0800

    1

--- ToDoList/Controllers/ToDoController.cs
+++ ToDoList/Controllers/ToDoController.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace ToDoList.Controllers
 {
-    [Authorize]
+    //[Authorize]
     public class ToDoController : Controller
     {
         private readonly ITodoItemService _todoItemService;
